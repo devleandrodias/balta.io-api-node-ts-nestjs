0785d00238e4768d15d468748450757e37da14ea
Leandro <leandrodbdias@gmail.com>
2020-04-17 17:30:07 -0300

    Conectando MySQL a aplicaçãi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
 import { StoreModule } from 'src/modules/store/store.module';
 
-const connectionString =
-  'mongodb+srv://ldbdias:QakMH0qhuLEtqLjT@cluster0-78woq.azure.mongodb.net/petshop';
-
 @Module({
   imports: [
-    MongooseModule.forRoot(connectionString),
+    MongooseModule.forRoot(
+      'mongodb+srv://ldbdias:QakMH0qhuLEtqLjT@cluster0-78woq.azure.mongodb.net/petshop',
+    ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'mySQL123',
+      database: 'petshop',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
     StoreModule,
   ],
