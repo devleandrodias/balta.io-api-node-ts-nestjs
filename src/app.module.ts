0eaba2ea1eb437858be4130a7e42747d6f13cff8
Leandro <leandrodbdias@gmail.com>
2020-04-17 14:48:48 -0300

    criação do módulo store e acrescentando typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 
 const connectionString =
   'mongodb+srv://ldbdias:QakMH0qhuLEtqLjT@cluster0-78woq.azure.mongodb.net/petshop';
 
 @Module({
-  imports: [MongooseModule.forRoot(connectionString), BackofficeModule],
+  imports: [
+    MongooseModule.forRoot(connectionString),
+    BackofficeModule,
+    StoreModule,
+  ],
 })
 export class AppModule {}
