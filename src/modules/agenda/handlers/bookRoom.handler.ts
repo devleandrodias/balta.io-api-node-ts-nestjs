e88ea7913b497183cf619bd8dbd9bf36b5be2fce
Leandro <leandrodbdias@gmail.com>
2020-04-20 15:35:23 -0300

    Adicionando o handler a aplicação

---
+++ src/modules/agenda/handlers/bookRoom.handler.ts
@@ -0,0 +1,17 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { BookRoomCommand } from '../commands/bookRoom.command';
+import { RoomRepository } from '../repositories/room.repository';
+
+@CommandHandler(BookRoomCommand)
+export class BookRoomHandler implements ICommandHandler<BookRoomCommand> {
+  constructor(private readonly repository: RoomRepository) {}
+
+  async execute(command: BookRoomCommand) {
+    console.log('BookRoomHandler:execute - Executando o comando...');
+
+    const room = await this.repository.findOneById(command.roomId);
+
+    room.book(command.customerId);
+    // commit()
+  }
+}
