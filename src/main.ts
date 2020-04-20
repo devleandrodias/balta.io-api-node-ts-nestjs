2814b989b137250301e54c94a69ebc67221f48bf
Leandro <leandrodbdias@gmail.com>
2020-04-20 14:38:10 -0300

    Adicionando compressão a aplciação

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomLogger } from './shared/services/logger';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger(),
   });
+
+  app.use(compression());
+
   await app.listen(4000);
 }
 bootstrap();
