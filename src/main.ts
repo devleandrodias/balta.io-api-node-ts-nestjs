31169dbd875bcf5077629196f2c9961819b84703
Leandro <leandrodbdias@gmail.com>
2020-04-20 14:57:32 -0300

    Adicionando documentação a API

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CustomLogger } from './shared/services/logger';
 import * as compression from 'compression';
@@ -9,6 +10,17 @@ async function bootstrap() {
   });
 
   app.use(compression());
+
+  const options = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API do curso de Node com Typescript')
+    .setVersion('1.0.0')
+    .addTag('petshop')
+    .build();
+
+  const documentation = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('', app, documentation);
 
   await app.listen(4000);
 }
