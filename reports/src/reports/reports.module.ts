b3d2baab592947e0f8245fb9b4f340c391b5c86f
Leandro <leandrodbdias@gmail.com>
2020-04-21 10:52:06 -0300

    remoção submodulo

--- reports/src/reports/reports.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReportService } from './services/reports.service';
-import { ReportsResolver } from './resolvers/reports.resolver';
-
-@Module({
-  providers: [ReportService, ReportsResolver],
-})
-export class ReportsModule {}
